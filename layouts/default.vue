71d9b02e0fc78287e9dc54507ba2aed7d49e7b8b
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-01-15 13:28:53 -0300

    chore: add AppMenu.vue component to default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <div class="content">
+      <AppMenu/>
       <Nuxt/>
     </div>
   </div>
 </template>
 
 <script>
+import AppMenu from "~/components/AppMenu";
 
 export default {
   components: {
+    AppMenu
   },
 };
 </script>
