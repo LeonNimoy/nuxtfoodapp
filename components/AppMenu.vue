aa87d4d85d31a650e1b587047be38700e1b82e1c
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-01-15 17:18:36 -0300

    feat: add restaurants to AppMenu.vue

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -4,6 +4,9 @@
       <li>
         <nuxt-link to="/">Home</nuxt-link>
       </li>
+      <li>
+        <nuxt-link to="/restaurants">Restaurants</nuxt-link>
+      </li>
     </ul>
   </nav>
 </template>
