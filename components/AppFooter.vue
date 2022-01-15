3251be6fe69ac6ec329bc9459db64f5b6b795ec6
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-01-15 17:17:29 -0300

    feat: create AppFooter.vue component

---
+++ components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>Copyright information goes here.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Appfooter"
+}
+</script>
+
+<style scoped>
+
+</style>
