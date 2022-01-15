44ccd830ec88b565540072d5ff0e519304ebc58d
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-01-15 17:17:47 -0300

    feat: create restaurants.vue page

---
+++ pages/restaurants.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="container restaurant">
+    <h1>Restaurants</h1>
+    <AppRestaurantinfo/>
+  </main>
+</template>
+
+<script>
+import AppRestaurantinfo from "~/components/AppRestaurantinfo";
+
+export default {
+  components: {
+    AppRestaurantinfo
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
