a2d52024984f7540708f19d77027c670315e04a0
Leonardo Cunha <lmscunha@sga.pucminas.br>
2022-01-24 06:34:48 -0300

    feat: [WIP]create AppSelect.vue component

--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -1,16 +1,28 @@
 <template>
   <main class="container restaurant">
-    <h1>Restaurants</h1>
+    <div class="restaurantheading">
+      <h1>Restaurants</h1>
+
+      <AppSelect @change="selectedRestaurant = $event"/>
+      <pre>{{ $data }}</pre>
+    </div>
     <AppRestaurantinfo/>
   </main>
 </template>
 
 <script>
 import AppRestaurantinfo from "~/components/AppRestaurantinfo";
+import AppSelect from "@/components/AppSelect";
 
 export default {
   components: {
-    AppRestaurantinfo
+    AppRestaurantinfo,
+    AppSelect
+  },
+  data() {
+    return {
+      selectedRestaurant: ''
+    }
   }
 };
 </script>
